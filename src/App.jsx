a11bb6f0e1a6d58edcfb1a39faf26f8955aafbcc
Poddyman0 <127142059+Poddyman0@users.noreply.github.com>
2023-09-11 15:34:00 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import "./App.css";
 import { Route, Routes} from "react-router-dom";
 import SignInForm from "./components/SignInForm"
 import Signup from "./components/Signup"
+import React, {useState, useEffect} from 'react';
+import { BrowserRouter as Router} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <>
-    <Routes>
-      <Route path="/LogInForm" element={<SignInForm />}></Route>
-      <Route path="/Signup" element={<Signup />}></Route>
-    </Routes>
-    </>
+  return (
+    <Router>
+      <div>
+        <section>                              
+            <Routes>                                                                        <Route path="/" element={<Home/>}/>
+              <Route path="/LogInForm" element={<SignInForm />}></Route>
+              <Route path="/Signup" element={<Signup />}></Route>
+            </Routes>                    
+        </section>
+      </div>
+    </Router>
   );
 }
 
