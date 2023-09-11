380461fb2a033e5a388ca97cc0f94b0f40b797b8
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-09-11 15:36:21 +0100

    App.jsx fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import "./App.css";
-import { Route, Routes} from "react-router-dom";
-import SignInForm from "./components/SignInForm"
-import Signup from "./components/Signup"
-import React, {useState, useEffect} from 'react';
-import { BrowserRouter as Router} from 'react-router-dom';
-import {Routes, Route} from 'react-router-dom';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import SignInForm from './components/SignInForm';
+import Signup from './components/Signup';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-  return (
     <Router>
       <div>
-        <section>                              
-            <Routes>                                                                        <Route path="/" element={<Home/>}/>
-              <Route path="/LogInForm" element={<SignInForm />}></Route>
-              <Route path="/Signup" element={<Signup />}></Route>
-            </Routes>                    
+        <section>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/LogInForm' element={<SignInForm />}></Route>
+            <Route path='/Signup' element={<Signup />}></Route>
+          </Routes>
         </section>
       </div>
     </Router>
