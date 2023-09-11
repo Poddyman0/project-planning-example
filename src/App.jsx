9ed89fdda864d445c673e53ef4c9ae61d684d657
RonaldB123 <ronaldbeck@hotmail.co.uk>
2023-09-11 15:24:14 +0100

    Routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1>Beat it!</h1>
+      <Routes>
+        <Route path="/" element={<p>home</p>}/>
+        <Route path="/signup" element={<p>signup</p>}/>
+        <Route path="/login" element={<p>login</p>}/>
+      </Routes>
     </>
   );
 }
