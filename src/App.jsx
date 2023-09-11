3c0d8a3b3048a2d34c850aa08d407789062a879d
Poddyman0 <127142059+Poddyman0@users.noreply.github.com>
2023-09-11 15:42:51 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import SignInForm from './components/SignInForm';
 import Signup from './components/Signup';
+import Home from './components/Home';
+import SignOut from './components/SignOut';
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
@@ -15,6 +17,8 @@ function App() {
             <Route path='/' element={<Home />} />
             <Route path='/LogInForm' element={<SignInForm />}></Route>
             <Route path='/Signup' element={<Signup />}></Route>
+            <Route path='/Home' element={<Home />}></Route>
+            <Route path='/SignOut' element={<SignOut />}></Route>
           </Routes>
         </section>
       </div>
