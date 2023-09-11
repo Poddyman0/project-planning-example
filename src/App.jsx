984379b391fc675536815fdf0af439031b0e44a0
Poddyman0 <127142059+Poddyman0@users.noreply.github.com>
2023-09-11 15:31:01 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Route, Routes} from "react-router-dom";
 import SignInForm from "./components/SignInForm"
+import Signup from "./components/Signup"
 
 function App() {
   return (
     <>
     <Routes>
       <Route path="/LogInForm" element={<SignInForm />}></Route>
+      <Route path="/Signup" element={<Signup />}></Route>
     </Routes>
     </>
   );
